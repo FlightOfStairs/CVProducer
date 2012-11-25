83b11578084f686964dbca5eb74cd81e7e1db751
Alistair Smith <alistairsmith@flightofstairs.org>
2012-11-25 22:59:57 +0000

    Build templates to files.

--- src/cvproducer/core.clj
+++ src/cvproducer/core.clj
@@ -3,10 +3,18 @@
   (:use cvproducer.alistairsmith)
   (:use fleet))
 
-(def templates ["template.html" "template.txt"])
-
 (defn get-resource [resource-path]
   (slurp (clojure.java.io/resource resource-path)))
 
+(def builds [ { :template "template.html" :output "alistair-smith.html" :meta { :style (get-resource "style.css" ) } }
+              { :template "template.txt"  :output "alistair-smith.txt"  :meta {} } ] )
+
+(defn do-build [build]
+  (spit
+    (:output build)
+    ((fleet [data meta] (get-resource (:template build))) cv (:meta build) )))
+
 (defn -main []
-  (println (str ((fleet [data meta] (get-resource (second templates))) cv {}))))
+  (dorun
+    (map do-build
+      builds)))

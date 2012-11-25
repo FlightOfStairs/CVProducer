4a1aa978d5b7699e1af65cbe5b0add72556c0ff8
Alistair Smith <alistairsmith@flightofstairs.org>
2012-11-25 21:23:00 +0000

    Updating main.

--- src/cvproducer/core.clj
+++ src/cvproducer/core.clj
@@ -1,4 +1,12 @@
-(ns cvproducer.core)
+(ns cvproducer.core
+  (:use cvproducer.html)
+  (:use cvproducer.alistairsmith)
+  (:use fleet))
+
+(def templates ["template.html" "template.txt"])
+
+(defn get-resource [resource-path]
+  (slurp (clojure.java.io/resource resource-path)))
 
 (defn -main []
-  (println "I solve a problem"))
+  (println (str ((fleet [data meta] (get-resource (second templates))) cv {}))))
